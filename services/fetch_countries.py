888404de248e8bc61f71d390124ca81a97ba685f
Prathamesh Ghatole <prathamesh.s.ghatole@gmail.com>
2023-09-26 19:42:11 +0530

    updated queries to include Geonames ID, and country status (for combined query)

--- services/fetch_countries.py
+++ services/fetch_countries.py
@@ -5,12 +5,15 @@ PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 PREFIX wikibase: <http://wikiba.se/ontology#>
 
-SELECT ?country ?countryLabel ?iso_3166_1_code
+SELECT ?country ?countryLabel ?iso_3166_1_code ?geonamesID
 WHERE {
   {
     ?country wdt:P31 wd:Q3624078.
     OPTIONAL {
       ?country wdt:P297 ?iso_3166_1_code.
+    }
+    OPTIONAL {
+      ?country wdt:P1566 ?geonamesID.
     }
   }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
@@ -22,31 +25,37 @@ PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 PREFIX wikibase: <http://wikiba.se/ontology#>
 
-SELECT ?country ?countryLabel ?iso_3166_3_code
+SELECT ?country ?countryLabel ?iso_3166_3_code ?geonamesID
 WHERE {
   {
     ?country wdt:P31 wd:Q3024240.
     OPTIONAL {
       ?country wdt:P773 ?iso_3166_3_code.
     }
+    OPTIONAL {
+      ?country wdt:P1566 ?geonamesID.
+    }
   }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 }
-
 """
 
-FORMER_CURRENT_COUNTRIES = """\
+FORMER_AND_CURRENT_COUNTRIES = """\
 PREFIX wd: <http://www.wikidata.org/entity/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
 PREFIX wikibase: <http://wikiba.se/ontology#>
 
-SELECT ?country ?countryLabel ?iso_3166_1_code ?iso_3166_3_code
+SELECT ?country ?countryLabel ?iso_3166_1_code ?iso_3166_3_code ?status ?geonamesID
 WHERE {
   {
     ?country wdt:P31 wd:Q3624078.
     OPTIONAL {
       ?country wdt:P297 ?iso_3166_1_code.
     }
+    OPTIONAL {
+      ?country wdt:P1566 ?geonamesID.
+    }
+    BIND("Current" AS ?status)
   }
   UNION
   {
@@ -54,13 +63,19 @@ WHERE {
     OPTIONAL {
       ?country wdt:P773 ?iso_3166_3_code.
     }
+    OPTIONAL {
+      ?country wdt:P1566 ?geonamesID.
+    }
+    BIND("Historical" AS ?status)
   }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 }
+
 """
 
 
 def main():
+    # fq.fetch_and_store(FORMER_AND_CURRENT_COUNTRIES, "data/former_and_current_countries.csv")
     fq.fetch_and_store(CURRENT_COUNTRIES, "data/current_countries.csv")
     fq.fetch_and_store(FORMER_COUNTRIES, "data/former_countries.csv")
 
